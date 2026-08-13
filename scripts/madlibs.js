4e567e338c43e750212eaee79cdbfd9a4765923e
Baby Soda <babysodacodex@users.noreply.github.com>
2026-08-12 20:49:03 -0500

    add a closing

---
+++ scripts/madlibs.js
@@ -0,0 +1,31 @@
+let style = prompt("Fancy or silly?");
+let name = prompt("A name:");
+let adjective = prompt("An adjective:");
+let noun = prompt("A noun:");
+
+
+let closings = [
+  "You are a legend.",
+  "Stay golden.",
+  "Signs point to awesome.",
+];
+
+function randomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+function makeCard() {
+  if (style === "fancy") {
+    console.log("Dearest " + name + ",");
+  } else {
+    console.log("YO " + name + "!");
+  }
+    console.log("Dear " + name + ",");
+  console.log("You are as " + adjective + " as a " + noun + ".");
+  let index = randomNumber(0, closings.length - 1);
+  console.log(closings[index]);
+}
+
+
+
+makeCard();
---
+++ scripts/tick.js
@@ -0,0 +1,14 @@
+let count = 0;
+let speed = 1000;
+let timer = setInterval(tick, speed);
+
+function tick() {
+  count = count + 1;
+  console.log("Tick " + count);
+
+  if (count === 10) {
+    clearInterval(timer);
+    console.log("The counter rests.");
+  }
+}
+
