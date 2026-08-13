71f5d5567c5a9e27ff1e7092c1e6cc58707b220d
Baby Soda <babysodacodex@users.noreply.github.com>
2026-08-12 21:49:26 -0500

    helper function

--- scripts/pause.js
+++ scripts/pause.js
@@ -2,7 +2,7 @@ function makeCard() {
   console.log("Dear Ada,");
   console.log("You are as brave as a volcano.");
 }
-
+//helperfunction
 console.log("Warming up the generator...");
 setTimeout(makeCard(), 3000);
 console.log("Generator loaded. Card in 3 seconds.");
